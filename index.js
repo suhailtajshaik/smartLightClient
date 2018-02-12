613ec97dc3d9fac74a471ace3f20a16a7c63371a
Suhail Taj Shaik <suhailtajshaik@gmail.com>
2018-02-12 01:02:08 -0500

    npm module changed to onoff

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
 const config = require('./config');
-
 const serverURL = config.serverProtocol + "://" + config.serverHost + ":" + config.serverPort;
 const socket = require('socket.io-client')(serverURL);
-var gpio = require('rpi-gpio');
-gpio.setup(4, gpio.DIR_OUT, write(4, true));
+const mapState = require("./util");
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
 
 process.on("SIGINT", () => {
-    gpio.write(4, true, () => {
-        gpio.distroy(() => {
-            process.exit();
-        });
-    });
+    LED.unexport();
+    process.exit();
 });
-
-function write(pinNo, value) {
-    // pinNo  =  GPIO pin no
-    // value  = true => OFF
-    // value  = false => ON
-    gpio.write(pinNo, value, function (err) {
-        if (err) throw err;
-        console.log('Written to pin ' + pinNo);
-    });
-}
 
 socket.on("connect", function () {
     console.log("RASPBERRY PI : Connected to " + serverURL);
     socket.on("updateState", function (state) {
         console.log("RASPBERRY PI Light is turned : ", state ? 'ON' : 'OFF');
-        write(4, !state);
+        LED.writeSync(mapState[state]);
     });
 });
---
+++ util.js
@@ -0,0 +1,8 @@
+const mapState = {
+    "true" : 1,
+    "false" : 0,
+    "1": true,
+    "0": false
+}
+
+module.exports = mapState;

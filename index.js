4559ad9444a75c2ca19368800f21bcba2d41827d
Suhail Taj Shaik <suhailtajshaik@gmail.com>
2018-02-14 19:16:24 -0500

    LED OFF fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const config = require('./config');
 const serverURL = config.serverProtocol + "://" + config.serverHost + ":" + config.serverPort;
-const socket = require('socket.io-client')(serverURL,{ reconnection: false });
+const socket = require('socket.io-client')(serverURL, { reconnection: false });
 const mapState = require("./util");
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 
@@ -14,17 +14,19 @@ socket.on("connect", function () {
     console.log("RASPBERRY PI : Connected to " + serverURL);
     socket.on("updateState", function (state) {
         console.log("RASPBERRY PI Light is turned : ", state ? 'ON' : 'OFF');
-        triggerIO(4,state);
+        triggerIO(4, state);
     });
 
-    socket.on('disconnect', function() {
+    socket.on('disconnect', function () {
         console.log("PI Disconnected");
         socket.emit("PiDisconnect");
     });
 });
 
-function triggerIO(pin,state) {
+function triggerIO(pin, state) {
     var LED = new Gpio(pin, 'out'); //use GPIO pin 4, and specify that it is output
     LED.writeSync(mapState[state]);
-    LED.unexport();
+    if (!state) {
+        LED.unexport();
+    }
 }

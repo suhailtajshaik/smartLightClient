5c2a753075d61a571bc9aee76003ba8b3d3c0ef8
Suhail Taj Shaik <suhailtajshaik@gmail.com>
2018-02-14 21:05:21 -0500

    LED Status

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const serverURL = config.serverProtocol + "://" + config.serverHost + ":" + conf
 const socket = require('socket.io-client')(serverURL, { reconnection: false });
 const mapState = require("./util");
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-
+var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+let isAlive = false;
 
 process.on("SIGINT", () => {
     socket.emit("PiDisconnect");
@@ -11,22 +12,18 @@ process.on("SIGINT", () => {
 });
 
 socket.on("connect", function () {
+    isAlive = true;
+    socket.emit("status", {"status": mapState[LED.read()]});
+
     console.log("RASPBERRY PI : Connected to " + serverURL);
     socket.on("updateState", function (state) {
         console.log("RASPBERRY PI Light is turned : ", state ? 'ON' : 'OFF');
-        triggerIO(4, state);
+        LED.writeSync(mapState[state]);
     });
 
     socket.on('disconnect', function () {
+        isAlive = false;
         console.log("PI Disconnected");
-        socket.emit("PiDisconnect");
+        socket.emit("PiDisconnect", { "isAlive": isAlive });
     });
 });
-
-function triggerIO(pin, state) {
-    var LED = new Gpio(pin, 'out'); //use GPIO pin 4, and specify that it is output
-    LED.writeSync(mapState[state]);
-    if (!state) {
-        LED.unexport();
-    }
-}
